e8857148fdcde5e1b236fe04a02822c8b79a5b98
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-14 20:59:24 +0530

    initialised application and added application properties to connect to mysql database

---
+++ src/main/java/com/tutorial/coviddashboard/CovidDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.coviddashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/coviddashboard/CovidDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.coviddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
