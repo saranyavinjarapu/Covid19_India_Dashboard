dab1b2db57093155c7ab4ac22a211342a9d57336
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-15 00:11:20 +0530

    connected dataservice to main controller

--- src/main/java/com/tutorial/coviddashboard/web/MainController.java
+++ src/main/java/com/tutorial/coviddashboard/web/MainController.java
@@ -1,11 +1,20 @@
 package com.tutorial.coviddashboard.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tutorial.coviddashboard.model.CovidData;
+import com.tutorial.coviddashboard.service.CovidDataService;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	CovidDataService covidDataService;
 
 	@GetMapping("/login")
 	public String login() {
@@ -13,7 +22,15 @@ public class MainController {
 	}
 
 	@GetMapping("/")
-    public String home() {
-        return "index";
+	public String home(Model model) {
+		List<CovidData> allStats = covidDataService.getAllStats();
+
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getConfirmed()).sum();
+
+		model.addAttribute("locationStats", allStats);
+		model.addAttribute("totalReportedCases", totalReportedCases);
+
+		return "index";
 	}
 }
+
